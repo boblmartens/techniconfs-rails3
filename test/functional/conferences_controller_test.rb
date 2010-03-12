d3fd3f8e1d5dbe9cb643863eb42f42806d320a3b
Bob Martens <boblmartens@gmail.com>
2010-03-12 13:59:53 -0600

    controller tests pass

--- test/functional/conferences_controller_test.rb
+++ test/functional/conferences_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class ConferencesControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+  context "a visitor" do
+    setup do
+      @conf = Factory.create(:conference)
+    end
+    
+    should "be able to view all currently created conferences" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:conferences)
+    end
+    
+    # should "not be able to add a conference" do
+    #   get :new
+    #   assert_redirected_to login_path
+    # end
   end
 end
